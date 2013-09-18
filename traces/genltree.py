da416e1b52336c8cf4e200c446e804f3c29762ae
kair <kair@cs.cmu.edu>
2013-09-17 21:40:27 -0400

    Add workload analysis section

--- traces/genltree.py
+++ traces/genltree.py
@@ -20,22 +20,26 @@ def gentree(cur, ndepth, f1, f2):
         f2.write(cur+'\n')
     if ndepth > option['maxdepth']:
         f1.write(cur+'\n');
-        f2.write(cur+'\n')
         return
-    for i in range(option['width']):
+    if (ndepth == option['maxdepth']):
+        width = option['lastwidth']
+    else:
+        width = option['width']
+    for i in range(width):
         fn = getFilename();
         gentree(cur+'/'+fn, ndepth+1, f1, f2)
 
 option = {
   'fnlen' : 12,
-  'maxdepth' : 6,
-  'width' : 8
+  'maxdepth' : 15,
+  'width' : 2,
+  'lastwidth': 2,
 }
 
-f1 = open("tree.log", "w")
-f2 = open("alltree.log", "w")
-f1.write("%d\n"%(math.pow(option['width'], option['maxdepth'])))
-f2.write("%d\n"%((math.pow(option['width'],option['maxdepth']+1)-1))/(option['wdith']-1))
+f1 = open("tree6.log", "w")
+f2 = open("alltree6.log", "w")
+f1.write("%d\n"%(math.pow(option['width'], option['maxdepth']-1)*option['lastwidth']))
+f2.write("%d\n"%((math.pow(option['width'],option['maxdepth']+1)-option['width'])/(option['width']-1)))
 gentree("", 1, f1, f2)
 f1.close()
 f2.close()

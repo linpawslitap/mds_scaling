9100c589d21a92b43d4a8f029d6b7d0536f13ce8
Kai Ren <kair@ko-ops.nmc-probe.org>
2013-09-17 21:29:20 -0600

    Add measurement class

---
+++ common/measure.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+* Author: Kai Ren
+* Created Time: 2013-09-17 13:21:12
+* File Name: ./measure.h
+* Description:
+ ************************************************************************/
+
+#ifndef MEASURE_H_
+#define MEASURE_H_
+
+
+void start_measure();
+void start_op();
+void finish_op();
+void finish_measure();
+
+#endif //MEASURE_H_
